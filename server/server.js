8ce80228fcadd76a03eb064562f33f0352e0f990
Cubites <cubite404@gmail.com>
2022-08-04 23:05:50 +0900

    naver search api practice

--- server/server.js
+++ server/server.js
@@ -17,6 +17,27 @@ const Mariadb = mariadb.createConnection({
     database: process.env.DB_DATABASE
 });
 
+
+let client_id = process.env.NAVER_CLIENT_ID;
+let client_secret = process.env.NAVER_CLIENT_SECRET;
+app.get('/search/book', function (req, res) {
+    let api_url = 'https://openapi.naver.com/v1/search/book.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1'; // json 결과
+    let request = require('request');
+    let options = {
+       url: api_url,
+       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret, 'Content-Type': 'application/json'}
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
 app.get('/home', (req, res) => {
     console.log('메인 페이지');
     axios.get('https://openapi.naver.com/v1/search/book.json?d_isbn=9791158392406')
