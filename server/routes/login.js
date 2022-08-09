5cacd33706366fc498cb53f7ef0ca77e1f769b8f
Cubites <75084369+Cubites@users.noreply.github.com>
2022-08-09 23:14:24 +0900

    신규 유저 체크 & db 저장

--- server/routes/login.js
+++ server/routes/login.js
@@ -19,12 +19,28 @@ const Mariadb = mariadb.createPool({
 
 // login
 router.post('/user/login', authenticated, (req, res, next) => {
-    console.log(req);
     Mariadb.getConnection()
         .then(conn => {
-            console.log(typeof(req.body.user_code));
             conn.query(`select id from user where ${req.body.login_method}_token = '${req.body.user_code}';`)
                 .then(data => {
+                    if(data.length == 0){ // 신규 유저 체크
+                        conn.query(`select max(id) as last_index from user;`)
+                            .then(data => {
+                                if(data[0].last_index == null){ // 사이트에 가입한 첫 유저인 경우
+                                    conn.query(`
+                                        insert into user (id, nickname, ${req.body.login_method}_token)
+                                            value (1, '${(Math.round(Math.random() * 1000000))}', '${req.body.user_code}');
+                                    `);
+                                }else{
+                                    conn.query(`
+                                        insert into user (id, nickname, ${req.body.login_method}_token)
+                                            value (${data[0].last_index + 1}, '${(Math.round(Math.random() * 1000000))}', '${req.body.user_code}');
+                                    `);
+                                }
+                            })
+                            .catch(err => res.status(500).send('id check err : ' + err));
+                        // next()
+                    }
                     res.status(200).send(data);
                 })
                 .catch(err => res.status(500).send('query error : ' + err));
