57a1de9cadf1a7ff69371912b7d78d0ebef05713
Cubites <75084369+Cubites@users.noreply.github.com>
2022-08-11 18:36:28 +0900

    신규 유저 확인 중 발생한 비동기 문제 처리중
    bcrypt.compare 비동기 문제 발생,
     bcrypt.compareSync로 교체중

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-// const dotenv = require('dotenv');
-const router = express.Router();
+const mariadb = require('mariadb');
 
+// const dotenv = require('dotenv');
 // dotenv.config({path: path.resolve(__dirname, '../.env')});
+
+const Mariadb = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    bigIntAsNumber: true
+});
+
+
 
 exports.authenticated = (plainPassword) => {
     bcrypt.genSalt(Number(process.env.SALT_ROUNDS), function (err, salt){
