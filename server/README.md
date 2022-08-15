edf93d38b11bdd953dd20fe1c9a9d43593b002b3
Cubites <cubite404@gmail.com>
2022-08-15 23:02:21 +0900

    token 작업

--- server/README.md
+++ server/README.md
@@ -1,27 +1,19 @@
-## config/config.json 생성 필요
-* 코드 예
-<pre>
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
-</pre>
+## .env 파일 생성
+* server 폴더(서버 최상위 폴더)에 .env 파일 생성
+  * 내용
+    <pre>
+      NAVER_CLIENT_ID=네이버_개발자_아이디
+      NAVER_CLIENT_SECRET=네이버_개발자_시크릿_코드
+      SALT_ROUNDS=솔트값
+      COOKIE_SECRET_KEY=쿠키_시크릿_키
+
+      DB_HOST=DB_HOST_경로
+      DB_USER=DB_유저명
+      DB_PASSWORD=DB_유저_비밀번호
+      DB_DATABASE=DB명
+
+      JWT_SECRET_KEY=JWT_시크릿_키
+      HASING_ALGO=JWT_해싱_알고리즘
+      EXPIRE_LEN=JWT_만료시간
+      PUBLISHER=토큰_발급자명
+    </pre>
--- server/routes/jwt.js
+++ server/routes/jwt.js
@@ -1,24 +1,49 @@
 const jwt = require('jsonwebtoken');
+const mariadb = require('mariadb');
 
 // login check
-const accessToken = (data) => {
+const accessToken = (data, secret) => {
     const payload = {
         user_id : data,
         role: 'user'
     };
 
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    return jwt.sign(payload, secret, {
         expiresIn: '10m'
     })
 };
 
-const refressToken = (data) => {
-    let expireDate = new Date(Date.now() + (3 * 60 * 1000));
+const refressToken = (data, secret) => {
+    let expireDate = new Date(Date.now() + (3 * 60 * 60 * 1000));
+    const payload = {
+        accessToken : data,
+        expire: expireDate
+    };
 
+    return {
+        refressToken: jwt.sign(payload, secret, {}),
+        expire: expireDate
+    };
 }
 
 const accessTokenVerify = (token, secret) => {
+    jwt.verify(token, secret, (err, decoded) => {
+        if(err){
+            // if(err.message == 'jwt expired'){
 
+            // }
+            console.log(err.message);
+            return null;
+        }
+        console.log(decoded);
+        return decoded;
+    });
+    return accessToken;
+    // try{
+    //     return jwt.verify(token, secret);
+    // } catch (err) {
+    //     return null;
+    // }
 }
 
-module.exports = { accessToken, refressToken }
+module.exports = { accessToken, refressToken, accessTokenVerify };
