689b92eeaafe42b1e62d90369daa08e228c36a44
Cubites <75084369+Cubites@users.noreply.github.com>
2022-08-25 20:37:07 +0900

    책 추가기능 추가
    불필요한 코드와 파일, 모듈 정리, 책 추가 기능 추가 중

--- server/server.js
+++ server/server.js
@@ -9,17 +9,14 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 dotenv.config();
-// app.use(cookieParser());
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 
 app.set('port', 4000);
 
 // Router
-// const login = require('./routers/login');
-// const tokenAuth = require('./routers/tokenAuth'); // 토큰 유효성 검사 router
-
 const poolLogin = require('./routers/poolLogin');
 const poolTokenAuth = require('./routers/poolTokenAuth');
+const addbook = require('./routers/addBook');
 
 // 로그인 토큰 유효성 검사
 app.use(poolTokenAuth);
@@ -35,27 +32,9 @@ app.post('/user/login', poolLogin, (req, res) => {
         .status(200).send({loginSuccess: true, data: req.body});
 });
 
-app.get('/search/book', (req, res) => {
-    console.log("서버입장");
-    console.log(req.query.name);
-    let options = {
-        method: 'get',
-        url: 'https://openapi.naver.com/v1/search/book.json',
-        headers: {
-            'X-Naver-Client-Id':process.env.NAVER_CLIENT_ID, 
-            'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET
-        },
-        params: { query: req.query.name }
-    };
 
-    axios(options)
-        .then((rs) => {
-            console.log("api 데이터 요청 성공");
-            res.status(200).send({ "data": rs.data.items });
-        })
-        .catch(err => res.send(err));
-    }
-);
+// 도서 추가
+app.get('/book/add', addbook);
 
 app.listen(app.get('port'), () => {
     console.log(`app listening on port ${app.get('port')}...`);
